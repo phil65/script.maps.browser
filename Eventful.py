2c41b57a5ddbc82686c7da26347817f606cfa33c
phil65 <philipptemminghoff@gmx.de>
2016-02-21 03:40:03 +0100

    cleanup

--- Eventful.py
+++ Eventful.py
@@ -3,6 +3,7 @@ import xbmcgui
 
 GOOGLEMAPS_KEY = 'AIzaSyBESfDvQgWtWLkNiOYXdrA9aU-2hv_eprY'
 EVENTFUL_KEY = 'Nw3rh3mXn8RhMQNK'
+BASE_URL = "http://api.eventful.com/json/"
 
 
 class Eventful():
@@ -11,48 +12,47 @@ class Eventful():
         pass
 
     def select_category(self):
-        url = "http://api.eventful.com/json/categories/list?app_key=%s" % (EVENTFUL_KEY)
-        results = Get_JSON_response(url, 7)
+        results = self.get_data(method="categories/list",
+                                cache_days=7)
         modeselect = ["All Categories"]
         for item in results["category"]:
             modeselect.append(cleanText(item["name"]))
         index = xbmcgui.Dialog().select("Choose Category", modeselect)
+        if index == -1:
+            return None
         if index > 0:
             return results["category"][index - 1]["id"]
         elif index > -1:
             return ""
-        else:
-            return None
 
     def get_eventlist(self, lat="", lon="", query="", category="", radius=30):
-        base_url = "http://api.eventful.com/json/events/search?image_sizes=large&include=price&units=km&page_size=26&sort_order=date&date=Future&app_key=%s" % (EVENTFUL_KEY)
-        url = '&where=%.8f,%.8f&within=%i' % (lat, lon, int(radius))
-        if query:
-            url = url + '&query=%s' % (query)
-        if category:
-            url = url + '&category=%s' % (category)
-        results = Get_JSON_response(base_url + url)
-        return self.handle_events(results['events']['event'])
-
-    def get_venuelist(self, lat="", lon="", query=""):
-        base_url = "http://api.eventful.com/json/events/search?image_sizes=large&include=price&units=km&page_size=26&sort_order=date&date=Future&app_key=%s" % (EVENTFUL_KEY)
-        url = '&where=%.8f,%.8f&within=%i' % (lat, lon, int(radius))
-        if query:
-            url = url + '&query=%s' % (query)
-        #  url = 'https://api.foursquare.com/v2/venues/search?ll=%.6f,%.8f&query=%s&limit=50&client_id=%s&client_secret=%s&v=20130815' % (self.lat, self.lon, "Food", foursquare_id, foursquare_secret)
-        results = Get_JSON_response(base_url + url)
+        params = {"image_sizes": "large",
+                  "include": "price",
+                  "units": "km",
+                  "page_size": 26,
+                  "sort_order": "date",
+                  "date": "Future",
+                  "where": "%.8f,%.8f" % (lat, lon),
+                  "within": int(radius),
+                  "query": query,
+                  "category": category}
+        results = self.get_data(method="events/search",
+                                params=params,
+                                cache_days=7)
         return self.handle_events(results['events']['event'])
 
     def get_venue_info(self, event_id=""):
-        base_url = "http://api.eventful.com/json/venues/get?app_key=%s" % (EVENTFUL_KEY)
-        url = '&id=%s' % (str(event_id))
-        results = Get_JSON_response(base_url + url)
+        results = self.get_data(method="venues/get",
+                                params={"id": event_id},
+                                cache_days=7)
         return self.handle_events(results['venue'])
 
     def get_event_info(self, event_id=""):
-        base_url = "http://api.eventful.com/json/events/get?app_key=%s" % (EVENTFUL_KEY)
-        url = '&id=%s&image_sizes=blackborder500,edpborder500' % (str(event_id))
-        results = Get_JSON_response(base_url + url)
+        params = {"id": event_id,
+                  "image_sizes": "blackborder500,edpborder500"}
+        results = self.get_data(method="events/get",
+                                params=params,
+                                cache_days=7)
         return self.handle_events(results['venue'])
 
     def handle_events(self, results):
@@ -68,13 +68,13 @@ class Eventful():
             lon = str(venue['longitude'])
             search_string = lat + "," + lon
             googlemap = 'http://maps.googleapis.com/maps/api/staticmap?&sensor=false&scale=2&maptype=roadmap&center=%s&zoom=13&markers=%s&size=640x640&key=%s' % (search_string, search_string, GOOGLEMAPS_KEY)
-            if ("image" in venue) and venue["image"]:
+            if "image" in venue and venue["image"]:
                 photo = venue["image"]["large"]["url"]
             else:
                 photo = ""
             #  start_time = venue["start_time"].split(" ")
             #  stop_time = venue["stop_time"].split(" ")
-            if (venue["start_time"] == venue["stop_time"]) or (venue["stop_time"] is None):
+            if venue["start_time"] == venue["stop_time"] or not venue["stop_time"]:
                 date = venue["start_time"]
             else:
                 date = venue["start_time"] + " - " + venue["stop_time"]
@@ -97,9 +97,20 @@ class Eventful():
                          "sortletter": chr(letter),
                          "lat": lat,
                          "lon": lon}
-            pin_string = pin_string + "&markers=color:blue%7Clabel:" + chr(letter) + "%7C" + lat + "," + lon
+            pin_string += "&markers=color:blue%7Clabel:%s%7C%s,%s" % (chr(letter), lat, lon)
             places_list.append(prop_list)
             count += 1
             letter += 1
         return places_list, pin_string
 
+    def get_data(self, method, params={}, cache_days=0.5):
+        params["app_key"] = EVENTFUL_KEY
+        # params = {k: v for k, v in params.items() if v}
+        params = dict((k, v) for (k, v) in params.iteritems() if v)
+        params = dict((k, unicode(v).encode('utf-8')) for (k, v) in params.iteritems())
+        url = "{base_url}{method}?{params}".format(base_url=BASE_URL,
+                                                   method=method,
+                                                   params=urllib.urlencode(params))
+        return get_JSON_response(url=url,
+                                 cache_days=cache_days,
+                                 folder="YouTube")
