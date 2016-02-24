fbadf1170896b75b17fca986e39449e8d4320682
phil65 <philipptemminghoff@gmx.de>
2016-02-24 06:19:07 +0100

    cleanup

--- resources/lib/ImageTags.py
+++ resources/lib/ImageTags.py
@@ -19,42 +19,26 @@ def get_exif_data(image):
     return exif_data
 
 
-def _get_if_exist(data, key):
-    if key in data:
-        return data[key]
-    return None
-
-
 def _convert_to_degrees(value):
     """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
-    d0 = value[0][0]
-    d1 = value[0][1]
-    d = float(d0) / float(d1)
-    m0 = value[1][0]
-    m1 = value[1][1]
-    m = float(m0) / float(m1)
-    s0 = value[2][0]
-    s1 = value[2][1]
-    s = float(s0) / float(s1)
-
+    d = float(value[0][0]) / float(value[0][1])
+    m = float(value[1][0]) / float(value[1][1])
+    s = float(value[2][0]) / float(value[2][1])
     return d + (m / 60.0) + (s / 3600.0)
 
 
 def get_lat_lon(exif_data):
     """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
-    lat = None
-    lon = None
-    if "GPSInfo" in exif_data:
-        gps_info = exif_data["GPSInfo"]
-        gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
-        gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
-        gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
-        gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
-        if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
-            lat = _convert_to_degrees(gps_latitude)
-            if gps_latitude_ref != "N":
-                lat = 0 - lat
-            lon = _convert_to_degrees(gps_longitude)
-            if gps_longitude_ref != "E":
-                lon = 0 - lon
+    if "GPSInfo" not in exif_data:
+        return None, None
+    gps_info = exif_data["GPSInfo"]
+    gps_lat = gps_info.get("GPSLatitude")
+    gps_lat_ref = gps_info.get('GPSLatitudeRef')
+    gps_lon = gps_info.get('GPSLongitude')
+    gps_lon_ref = gps_info.get('GPSLongitudeRef')
+    if gps_lat and gps_lat_ref and gps_lon and gps_lon_ref:
+        lat = _convert_to_degrees(gps_lat)
+        lat = -lat if gps_lat_ref != "N" else lat
+        lon = _convert_to_degrees(gps_lon)
+        lon = -lon if gps_lon_ref != "E" else lon
     return lat, lon
